f5123320e82c67099df98228bff2a17d702e4d0b
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-05-27 12:11:03 -0400

    feat(mock-storage): implement mock-storage

---
+++ README.md
@@ -0,0 +1,35 @@
+# Storage Mock
+
+> In memory representation of Storage Interface
+
+100% compliant LocalStorage implement implementing TypeScript's `Storage` interface.
+This library makes use of ES2015 `Proxy` and `Map`, polyfilled may be required.
+
+## Get it
+```sh
+yarn add --dev mock-storage
+# or
+npm install --save-dev mock-storage
+```
+
+## API and Usage
+
+### `mockStorage(map?: Map<string, string>): Storage`
+
+Creates an in-memory version of LocalStorage, optionally with starting data.
+
+```typescript
+import { mockStorage } from 'mock-storage'
+
+const storage = mockStorage()
+
+// set item
+storage.setItem('foo', 'bar')
+// get item
+console.log(storage.getItem('foo')) // -> bar
+
+// Using property access
+storage.foo = 'baz'
+console.log(storage.foo) // -> baz
+
+```
---
+++ src/mockStorage.ts
@@ -0,0 +1,75 @@
+export function mockStorage(map?: Map<string, string>): Storage {
+  return new Proxy(new MockStorage(map), { get, set }) as MockedStorage
+}
+
+type MockedStorage =
+  MockStorage &
+  { [index: number]: string } &
+  { [key: string]: string }
+
+class MockStorage {
+  public map: Map<string, string>
+  public length: number
+
+  constructor(map?: Map<string, string>) {
+    this.map = map || new Map<string, string>()
+    this.updateLength()
+  }
+
+  public clear(): void {
+    this.map.clear()
+    this.updateLength()
+  }
+
+  public setItem(key: string, value: string) {
+    this.map.set(key, value)
+    this.updateLength()
+  }
+
+  public getItem(key: string): string | null {
+    return this.map.get(key) || null
+  }
+
+  public key(index: number): string | null {
+    const values = Array.from(this.map.values())
+
+    return values[index] || null
+  }
+
+  public removeItem(key: string) {
+    this.map.delete(key)
+    this.updateLength()
+  }
+
+  private updateLength() {
+    this.length = this.map.size
+  }
+}
+
+function get(target: MockStorage, property: string) {
+  if ((target as any)[property])
+    return (target as any)[property]
+
+  const int = parseInt(property, 10)
+
+  if (!Number.isNaN(int))
+    return target.key(int)
+
+  return target.getItem(property)
+}
+
+function set(target: MockStorage, property: PropertyKey, value: string) {
+  let key = property.toString()
+  const int = parseInt(key, 10)
+
+  if (!Number.isNaN(int)) {
+    const keys = Array.from(target.map.keys())
+
+    if (keys[int])
+      key = keys[int]
+  }
+
+  target.setItem(key, value)
+
+  return true
+}
